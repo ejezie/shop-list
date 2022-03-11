7926d11ab758c259baa5918a8e65b5c52143c670
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-11 15:41:15 +0100

    adding data fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import List from './containers/List';
+import Lists from './containers/Lists';
+import Form from './containers/Form';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      React
-    </div>
-  );
-}
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <AppWrapper>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Lists/>} />
+          <Route path='/list/:id/new' element={<Form/>} />
+          <Route path='/list/:id' element={<List/>} />
+        </Routes>
+      </Router>
+    </AppWrapper>
+  </>
+);
 
 export default App;
